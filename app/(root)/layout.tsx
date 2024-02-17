7a592e984bcdaf12734a16c3608e11717e94448e
ThomasJanko <thomas78125@gmail.com>
2024-02-17 11:34:40 +0100

    init nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='root'>
+      <Sidebar/>
+      {/* MobilNav */}
       <div className='root-container'>
         <div className='wrapper'>
           {children}
